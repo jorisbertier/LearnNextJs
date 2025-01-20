94a8dffc6075bbde27fbba2ed950fe689395b4e5
Joris Bertier <joris.bertier@gmail.com>
2025-01-20 14:16:06 -0500

    Setup metadata

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,18 @@
+import React from 'react';
 import Link from 'next/link';
 
-export default function NotFound() {
+const NotFound = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>404 - Page non trouvée</h1>
-      <p>Oups ! La page que vous recherchez n'existe pas.</p>
-      <Link href="/">
-          Retour à l'accueil
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1 style={{ fontSize: '3rem', marginBottom: '20px' }}>404 - Page Not Found</h1>
+      <p style={{ fontSize: '1.25rem', marginBottom: '20px' }}>
+        Désolé, la page que vous recherchez n'existe pas ou a été déplacée.
+      </p>
+      <Link href="/" style={{ fontSize: '1rem', color: '#0070f3', textDecoration: 'underline' }}>
+        Retour à l'accueil
       </Link>
     </div>
   );
-}
+};
+
+export default NotFound;
--- app/product/[idProduct]/page.tsx
+++ app/product/[idProduct]/page.tsx
@@ -1,10 +1,23 @@
 import Link from 'next/link'
+import { notFound } from 'next/navigation'
 import React from 'react'
+import { Metadata } from 'next'
+
+export const generateMetadata =({params} : { params: { idProduct: string }}) : Metadata => {
+    const { idProduct } = params;
+    return {
+        title:`Product ${idProduct}`
+    }
+}
 
 export default async function Product({params} : {
     params: Promise<{idProduct: string}>
 }) {
     const idProduct = (await params).idProduct
+
+    if(parseInt(idProduct) > 50) {
+        notFound()
+    }
     return (
         <>
             <div>Product {idProduct}</div>
